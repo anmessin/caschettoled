02ee01f2948105d0e3eb81a201c0e4f377c5a3d3
Antonio Messina <antonio.messina@na.infn.it>
2026-07-28 17:01:07 +0200

    feat(cli): add host selection via set host <n>

--- apps/cli/main.py
+++ apps/cli/main.py
@@ -1,5 +1,6 @@
 import os
 import cmd2
+import sys
 
 from dotenv import load_dotenv
 from commands import LittleHardHatCLI
@@ -31,12 +32,20 @@ def banner(host, timeout):
 def main():
 
     parser = cmd2.Cmd2ArgumentParser()
+    parser.add_argument('--board', type=int, default=1)
     parser.add_argument('--timeout', type=int, default=2)
     args = parser.parse_args()
 
     load_dotenv()
-    host = os.getenv("LHHBOARD3_IP")
+
+    env_key = f"LHHBOARD{args.board}_IP"
+    host = os.getenv(env_key)
     board = LittleHardHat(host, args.timeout)
+
+    if not host:
+        raise ValueError(f"Board {args.board} not found.")
+
+    sys.argv = [sys.argv[0]]
 
     banner(host, args.timeout)
     cli = LittleHardHatCLI(board)
